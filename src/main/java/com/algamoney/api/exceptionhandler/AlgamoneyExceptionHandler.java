483ce53c8c3f40574dad6ef3f2887bbb994316a9
luiscarlos <luiscarlos8pe@gmail.com>
2023-12-11 21:54:19 -0300

    Removendo pessoa com DELETE

--- src/main/java/com/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -23,81 +23,75 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
-		
-		@Autowired
-		private MessageSource messageSource;
-		
-		
-		
-		//Verifica as propiedades extras
-		@Override
-		protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-				HttpHeaders headers, HttpStatus status, WebRequest request) {
-			
-			 String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-			 String mensagemDesenvolvedor = ex.getCause().toString();
-			
-			 List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
-			return handleExceptionInternal(ex, erros , headers, HttpStatus.BAD_REQUEST, request);
-		}
-		
-		// valida quando o argumento e null sem poder ser 
-		@Override
-		protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+
+	@Autowired
+	private MessageSource messageSource;
+
+	// Verifica as propiedades extras
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-			
-			List<Erro> erros = criarListaDeErros(ex.getBindingResult());
-		    return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.getCause().toString();
+
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	// valida quando o argumento e null sem poder ser
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+
+	}
 	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	private void handleEmptyResultDataAccessException() {
+		
+	}
+	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
+		List<Erro> erros = new ArrayList<>();
+
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null,
+					LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = fieldError.toString();
+			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		}
-		private List<Erro>criarListaDeErros(BindingResult bindingResult){
-			List<Erro> erros = new ArrayList<>();
-			
-			for(FieldError fieldError: bindingResult.getFieldErrors()) {
-				String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-				String mensagemDesenvolvedor = fieldError.toString();
-				erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
-			}
-			
-			return erros;
+
+		return erros;
+	}
+
+	private static class Erro {
+
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+
+		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
 		}
-		private static class Erro{
-			
-			private String mensagemUsuario;
-			private String mensagemDesenvolvedor;
-			
-			public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
-				this.mensagemUsuario = mensagemUsuario;
-				this.mensagemDesenvolvedor = mensagemDesenvolvedor;
-			}
 
-
-
-			public String getMensagemUsuario() {
-				return mensagemUsuario;
-			}
-
-
-
-			public void setMensagemUsuario(String mensagemUsuario) {
-				this.mensagemUsuario = mensagemUsuario;
-			}
-
-
-
-			public String getMensagemDesenvolvedor() {
-				return mensagemDesenvolvedor;
-			}
-
-
-
-			public void setMensagemDesenvolvedor(String mensagemDesenvolvedor) {
-				this.mensagemDesenvolvedor = mensagemDesenvolvedor;
-			}
-			
-			
-			
-			
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
 		}
-	
+
+		public void setMensagemUsuario(String mensagemUsuario) {
+			this.mensagemUsuario = mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+
+		public void setMensagemDesenvolvedor(String mensagemDesenvolvedor) {
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
+
+	}
 }
