ed3f5df6c82ae7c97a0835fef7d8a2a0d9bd0195
luiscarlos <luiscarlos8pe@gmail.com>
2024-07-26 07:55:33 -0300

    Criando validação para pessoa que não existe

--- src/main/java/com/exemplo/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/exemplo/algamoneyapi/resource/PessoaResource.java
@@ -19,11 +19,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -31,7 +33,7 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
@@ -47,14 +49,25 @@ public class PessoaResource {
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
-	
+
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
 		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
-		return pessoa.isPresent() ? 
-		        ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
-		}
+		return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
 	}
-	
 
-
+    //@DeleteMapping("/{nome}")
+	public ResponseEntity<Void> deletar(@PathVariable Long codigo){
+		if(!pessoaRepository.existsById(codigo)) {
+			return ResponseEntity.notFound().build();
+		}
+		pessoaRepository.deleteById(codigo);
+		return ResponseEntity.noContent().build();
+	}
+    
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+    this.pessoaRepository.deleteById(codigo);
+    }
+}
